1d32892df24a4e97dec2d585c8d41484fa4f166d
Mario Hilsenrath <mario.hilsenrath@gmail.com>
2018-11-14 16:16:51 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  Restaurant.create( {name: "Jam", address: "Horacio Laffer", phone_number: "01 02 03", category: "japanese"})
+  Restaurant.create( {name: "Sicilia", address: "Jardins", phone_number: "02 02 03", category: "italian"})
+  Restaurant.create( {name: "Chi Fu", address: "Liberdade", phone_number: "03 02 03", category: "chinese"})
+  Restaurant.create( {name: "mani", address: "pinheiros", phone_number: "04 02 03", category: "french"})
+  Restaurant.create( {name: "waffle", address: "centro", phone_number: "05 02 03", category: "belgian"})
+
